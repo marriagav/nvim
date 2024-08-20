7bfe67579b4acb0804b6c483e7289dda83282ebe
Miguel Arriaga Velasco <marriagav3@gmail.com>
2024-08-19 23:37:19 -0600

    less context lines

--- lua/marriagav/plugins/treesitter.lua
+++ lua/marriagav/plugins/treesitter.lua
@@ -43,6 +43,9 @@ return { -- Highlight, edit, and navigate code
 		--    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
 		--    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
 		--    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
-		require("treesitter-context").setup()
+		require("treesitter-context").setup({
+			enable = true,
+			max_lines = 5,
+		})
 	end,
 }
