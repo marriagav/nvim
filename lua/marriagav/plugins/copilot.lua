644f31d0573a56e2d23e66ab26c8f1cc815dc9f5
Miguel Arriaga Velasco <marriagav3@gmail.com>
2024-09-21 16:54:50 -0400

    remap copilot

--- lua/marriagav/plugins/copilot.lua
+++ lua/marriagav/plugins/copilot.lua
@@ -1,3 +1,7 @@
 return {
 	"github/copilot.vim",
+	config = function()
+		vim.api.nvim_set_keymap("i", "<C-l>", 'copilot#Accept("<CR>")', { expr = true, noremap = true, silent = true })
+		vim.g.copilot_no_tab_map = true
+	end,
 }
