9228692238b21ab67658474fe6a21d169dbd3853
Miguel Arriaga Velasco <marriagav3@gmail.com>
2025-03-28 19:39:17 -0400

    nvim tree improvements'

--- lua/marriagav/plugins/nvimtree.lua
+++ lua/marriagav/plugins/nvimtree.lua
@@ -8,32 +8,41 @@ return {
 		vim.g.loaded_netrw = 1
 		vim.g.loaded_netrwPlugin = 1
 
+		local HEIGHT_RATIO = 0.8 -- You can change this
+		local WIDTH_RATIO = 0.5 -- You can change this too
+
 		nvimtree.setup({
-			view = {
-				width = 35,
-			},
-			-- change folder arrow icons
-			renderer = {
-				root_folder_label = false,
-				group_empty = false,
-			},
-			-- disable window_picker for
-			-- explorer to work well with
-			-- window splits
-			actions = {
-				open_file = {
-					window_picker = {
-						enable = false,
-					},
-				},
-			},
-			filters = {
-				custom = { ".DS_Store" },
-			},
-			git = {
-				ignore = false,
-			},
+			disable_netrw = true,
+			hijack_netrw = true,
+			respect_buf_cwd = true,
 			sync_root_with_cwd = true,
+			view = {
+				relativenumber = true,
+				float = {
+					enable = true,
+					open_win_config = function()
+						local screen_w = vim.opt.columns:get()
+						local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+						local window_w = screen_w * WIDTH_RATIO
+						local window_h = screen_h * HEIGHT_RATIO
+						local window_w_int = math.floor(window_w)
+						local window_h_int = math.floor(window_h)
+						local center_x = (screen_w - window_w) / 2
+						local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
+						return {
+							border = "rounded",
+							relative = "editor",
+							row = center_y,
+							col = center_x,
+							width = window_w_int,
+							height = window_h_int,
+						}
+					end,
+				},
+				width = function()
+					return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
+				end,
+			},
 		})
 
 		-- set keymaps
