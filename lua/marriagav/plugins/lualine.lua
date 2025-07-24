3719009d20e9fb1b9368b7cae2a4d0892c127446
Miguel Arriaga Velasco <marriagav3@gmail.com>
2025-07-24 19:56:17 -0400

    lualine

--- lua/marriagav/plugins/lualine.lua
+++ lua/marriagav/plugins/lualine.lua
@@ -14,31 +14,67 @@ return {
 			end
 			return prefix .. " " .. session_name
 		end
+		local mocha = require("catppuccin.utils.lualine")("mocha")
+		local fg_color = "fg"
+		local bg_color = mocha.inactive.a.bg
 		lualine.setup({
 			options = {
 				component_separators = { left = "|", right = "|" },
-				section_separators = { left = "", right = "" },
+				section_separators = { left = "|", right = "|" },
 			},
 
 			sections = {
-				lualine_x = {
+				lualine_a = {
 					{
-						lazy_status.updates,
-						cond = lazy_status.has_updates,
-						color = { fg = "#ff9e64" },
+						"mode",
+						color = function()
+							local current_mode = vim.api.nvim_get_mode().mode
+							local fg = mocha.normal.b.fg
+							local visual_modes = { v = true, V = true, ["\22"] = true }
+							local replace_modes = { R = true, ["Rv"] = true }
+							local select_modes = { s = true, S = true, ["\19"] = true }
+							if current_mode == "i" then
+								fg = mocha.insert.b.fg
+							elseif visual_modes[current_mode] then
+								fg = mocha.visual.b.fg
+							elseif replace_modes[current_mode] then
+								fg = mocha.replace.b.fg or "#ff0000" -- fallback color if mocha doesn't have replace
+							elseif select_modes[current_mode] then
+								fg = mocha.select.b.fg or "#ffaa00" -- fallback color
+							elseif current_mode == "c" then
+								fg = mocha.command.b.fg or "#00ffaa" -- fallback color
+							elseif current_mode == "t" then
+								fg = mocha.terminal.b.fg or "#00aaff" -- fallback color
+							else
+								fg = mocha.normal.b.fg
+							end
+							return { fg = fg, bg = bg_color, gui = "bold" }
+						end,
 					},
-					{ current_session },
-					{ "encoding" },
-					{ "filetype" },
+				},
+				lualine_b = {
+					{ "branch", color = { bg = bg_color } },
+					{ "diff", color = { bg = bg_color } },
+					{ "diagnostics", color = { bg = bg_color } },
 				},
 				lualine_c = {
 					{
 						"filename",
 						path = 1,
+						color = { fg = fg_color, bg = bg_color },
 					},
 				},
+				lualine_x = {
+					{
+						lazy_status.updates,
+						cond = lazy_status.has_updates,
+						color = { fg = "#ff9e64", bg = bg_color },
+					},
+					{ current_session, color = { fg = fg_color, bg = bg_color } },
+				},
+				lualine_y = { { "progress", color = { fg = fg_color, bg = bg_color } } },
+				lualine_z = { { "location", color = { fg = fg_color, bg = bg_color } } },
 			},
 		})
 	end,
 }
-
