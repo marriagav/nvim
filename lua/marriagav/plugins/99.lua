8339136ab04de136e1ce1f6142a44e3b6cd06d4a
Miguel Arriaga Velasco <marriagav3@gmail.com>
2026-03-29 14:17:58 -0400

    nvim

--- lua/marriagav/plugins/99.lua
+++ lua/marriagav/plugins/99.lua
@@ -6,38 +6,43 @@ return {
 		-- local cwd = vim.uv.cwd()
 		_99.setup({
 			-- From https://models.dev/
-			-- TODO: uncomment after https://github.com/ThePrimeagen/99/issues/98
+			provider = _99.Providers.OpenCodeProvider,
 			model = "github-copilot/claude-opus-4.6",
 
-			--- TODO: uncomment and configure after blink support is added: https://github.com/ThePrimeagen/99/issues/61
-			--- A new feature that is centered around tags
 			-- completion = {
-			-- 	--- Defaults to .cursor/rules
-			-- 	-- I am going to disable these until i understand the
-			-- 	-- problem better.  Inside of cursor rules there is also
-			-- 	-- application rules, which means i need to apply these
-			-- 	-- differently
-			-- 	-- cursor_rules = "<custom path to cursor rules>"
+			--   -- I am going to disable these until i understand the
+			--   -- problem better.  Inside of cursor rules there is also
+			--   -- application rules, which means i need to apply these
+			--   -- differently
+			--   -- cursor_rules = "<custom path to cursor rules>"
 			--
-			-- 	--- A list of folders where you have your own SKILL.md
-			-- 	--- Expected format:
-			-- 	--- /path/to/dir/<skill_name>/SKILL.md
-			-- 	---
-			-- 	--- Example:
-			-- 	--- Input Path:
-			-- 	--- "scratch/custom_rules/"
-			-- 	---
-			-- 	--- Output Rules:
-			-- 	--- {path = "scratch/custom_rules/vim/SKILL.md", name = "vim"},
-			-- 	--- ... the other rules in that dir ...
-			-- 	---
-			-- 	custom_rules = {
-			-- 		"scratch/custom_rules/",
-			-- 	},
+			--   --- A list of folders where you have your own SKILL.md
+			--   --- Expected format:
+			--   --- /path/to/dir/<skill_name>/SKILL.md
+			--   ---
+			--   --- Example:
+			--   --- Input Path:
+			--   --- "scratch/custom_rules/"
+			--   ---
+			--   --- Output Rules:
+			--   --- {path = "scratch/custom_rules/vim/SKILL.md", name = "vim"},
+			--   --- ... the other rules in that dir ...
+			--   ---
+			--   custom_rules = {
+			--     "scratch/custom_rules/",
+			--   },
 			--
-			-- 	--- What autocomplete do you use.  We currently only
-			-- 	--- support cmp right now
-			-- 	source = "cmp",
+			--   --- Configure @file completion (all fields optional, sensible defaults)
+			--   files = {
+			--     -- enabled = true,
+			--     -- max_file_size = 102400,     -- bytes, skip files larger than this
+			--     -- max_files = 5000,            -- cap on total discovered files
+			--     -- exclude = { ".env", ".env.*", "node_modules", ".git", ... },
+			--   },
+			--
+			--   --- What autocomplete do you use.  We currently only
+			--   --- support cmp right now
+			--   source = "blink",
 			-- },
 
 			--- WARNING: if you change cwd then this is likely broken
@@ -51,19 +56,25 @@ return {
 			--- assuming that /foo is project root (based on cwd)
 			md_files = {
 				"AGENT.md",
+				"CLAUDE.md",
 			},
 		})
 
 		-- Keymaps
-		vim.keymap.set("n", "<leader>9f", function()
-			_99.fill_in_function()
-		end, { desc = "[9]9 [F]ill in function" })
 		vim.keymap.set("v", "<leader>9v", function()
-			_99.visual_prompt({})
+			_99.visual({})
 		end, { desc = "[9]9 [V]isual prompt" })
 
-		vim.keymap.set("v", "<leader>9s", function()
+		vim.keymap.set("v", "<leader>9x", function()
 			_99.stop_all_requests()
-		end, { desc = "[9]9 [S]top all requests" })
+		end, { desc = "[9]9 [X] Stop all requests" })
+
+		vim.keymap.set("n", "<leader>9s", function()
+			_99.search()
+		end, { desc = "[9]9 [S]earch" })
+
+		vim.keymap.set("n", "<leader>9m", function()
+			require("99.extensions.telescope").select_model()
+		end, { desc = "[9]9 [M]odel selector" })
 	end,
 }
--- lua/marriagav/plugins/theme.lua
+++ lua/marriagav/plugins/theme.lua
@@ -1,25 +1,10 @@
--- Catpuccin
--- return {
--- 	"catppuccin/nvim",
--- 	name = "catppuccin",
--- 	priority = 1000,
--- 	opts = {
--- 		transparent_background = true,
--- 		float = { transparent = true, solid = false },
--- 	},
--- 	init = function()
--- 		vim.cmd.colorscheme("catppuccin-mocha")
--- 		vim.cmd.hi("Comment gui=none")
--- 	end,
--- }
-
 -- Koda
 return {
 	"oskarnurm/koda.nvim",
 	lazy = false, -- make sure we load this during startup if it is your main colorscheme
 	priority = 1000, -- make sure to load this before all the other start plugins
 	config = function()
-		require("koda").setup({ transparent = true })
+		require("koda").setup({})
 		vim.cmd("colorscheme koda")
 	end,
 }
